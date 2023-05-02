546de23524d5f4b34714c02ae13b7adf9279c634
CalebStephens <calebtaylorstephens@gmail.com>
2023-05-02 21:57:15 +1200

    fix(lint): lint failure fixes

--- hacker-news/pages/index.js
+++ hacker-news/pages/index.js
@@ -1,14 +1,14 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import Link from 'next/link'
-
-const inter = Inter({ subsets: ["latin"] });
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="card-display-parent center">
-      <Link href="./stories" className="card-parent"><h1>Go To Stories</h1></Link>
-      <Link href="./leaders" className="card-parent"><h1>Go To Leaders</h1></Link>
+      <Link href="./stories" className="card-parent">
+        <h1>Go To Stories</h1>
+      </Link>
+      <Link href="./leaders" className="card-parent">
+        <h1>Go To Leaders</h1>
+      </Link>
     </div>
   );
 }
