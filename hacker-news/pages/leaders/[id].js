89adc55df9823cd1ca18b3a0fceba51656687655
CalebStephens <calebtaylorstephens@gmail.com>
2023-05-02 20:34:58 +1200

    refactor(leader layout): changed layout of how user sees info

--- hacker-news/pages/leaders/[id].js
+++ hacker-news/pages/leaders/[id].js
@@ -2,28 +2,31 @@ import axios from "axios";
 import { useRouter } from "next/router";
 import ErrorPage from "next/error";
 import leaders from "@/json/leaders";
+import TimeConverter from "@/utils/timeConverter";
 
 const Leader = ({ account }) => {
   const router = useRouter();
   const { id } = router.query;
+  const MAXSUBMITTED = 10;
   return (
     <>
       {leaders.names.includes(id) ? (
         <div>
-          <h1>User {id}</h1>
-          <p>about: {account.about}</p>
-          <p>created: {account.created}</p>
-          <p>id: {account.id}</p>
-          <p>karma: {account.karma}</p>
-          {account.submitted.slice(0, 5).map((kid, index) => {
+          <h1>User: {id}</h1>
+          <p>About: {account.about}</p>
+          <p>Created: {TimeConverter(account.created).props.children}</p>
+          <p>ID: {account.id}</p>
+          <p>Karma: {account.karma}</p>
+          <div>Submitted:
+          {account.submitted.slice(0, MAXSUBMITTED).map((kid, index) => {
             return (
-              <p key={index}>
+              <p key={index}>&emsp;
                 <a
                   href={`https://hacker-news.firebaseio.com/v0/item/${kid}.json?print=pretty`}
                 >{`https://hacker-news.firebaseio.com/v0/item/${kid}.json?print=pretty`}</a>
               </p>
             );
-          })}
+          })}</div>
         </div>
       ) : (
         <ErrorPage statusCode={404} />
